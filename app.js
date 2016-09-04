b1e077b8700b70e398c168596302d95170719f92
MWP2 <mwp2@emag.ro>
2016-09-04 05:05:15 +0000

    commit

--- app.js
+++ app.js
@@ -16,7 +16,65 @@ app.config(function ($routeProvider) {
         .otherwise({redirectTo: '/'});
 });
 
-app.controller('HomeController', function ($scope, $http) {
+app.filter('price', function () {
+    return function (text, classCss, strike, feedback) {
+        var ret;
+        var strikeOpen = typeof strike !== 'undefined' ? '<s>': '';
+        var strikeClose = typeof strike !== 'undefined' ? '</s>': '';
+        var feedbackHTML = typeof feedback !== 'undefined' ? '<span class="product-this-deal">(-'+feedback+'%)</span>' : '';
+        if(typeof text !== 'undefined') {
+            text = text.toString().split('.');
+            ret = '<p class="'+classCss+'">'+strikeOpen+'' + text[0] + '<sup>' + text[1] + '</sup> <span>Lei</span>'+strikeClose+'</p>';
+        } else {
+            ret = '';
+        }
+        return ret;
+    };
+});
+
+app.controller('HomeController',['$scope', '$http', '$sce', '$routeParams',
+    function ($scope, $http, $sce, $routeParams) {
+        $scope.renderHtml = function (htmlCode) {
+            return $sce.trustAsHtml(htmlCode);
+        };
+
+        var resorcesToLoad = [
+            ['live-widget', 'json'],
+            ['slider', 'json'],
+            ['widgets', 'json'],
+            ['menu', 'html']
+        ];
+
+        for (var res in resorcesToLoad) {
+            requestData("homepage/" + resorcesToLoad[res][0] + "." + resorcesToLoad[res][1]).then(function (data) {
+                res = getResourceNameFromURL(data.resource);
+                $scope.$apply(function(){
+                    $scope[res] = data.data;
+                });
+                if(res=='menu'){
+                    loadScript("index_files/homepage.js");
+                }
+            }, function (err) {
+                var responsePromise = $http.get('http://api.hack1.smart-things.ro/' + err.resource);
+                responsePromise.success(function (data, status, headers, config) {
+                    res = getResourceNameFromURL(config.url);
+                    $scope[res] = data;
+                    saveData(err.resource, data);
+                    if(res=='menu'){
+                        loadScript("index_files/homepage.js");
+                    }
+                });
+                responsePromise.error(function (data, status, headers, config) {
+                    alert("AJAX failed!");
+                });
+            });
+        }
+
+
+
+
+    }]
+);
     // var responsePromise = $http.get("https://sapi.emag.ro/products/DVBTMMBBM?source_id=7");
 
     // responsePromise.success(function(data, status, headers, config) {
@@ -26,8 +84,8 @@ app.controller('HomeController', function ($scope, $http) {
     //     alert("AJAX failed!");
     // });
 
-    loadScript("index_files/homepage.js");
-});
+//     loadScript("index_files/homepage.js");
+// });
 
 
 app.controller('ProductController',['$scope', '$http', '$sce', '$routeParams',
