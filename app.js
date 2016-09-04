09b0c1be7aa3e0892d521a5ea5be1b90c5a01f01
MWP2 <mwp2@emag.ro>
2016-09-04 00:13:12 +0000

    Add html resource type

--- app.js
+++ app.js
@@ -30,33 +30,43 @@ app.controller('HomeController', function ($scope, $http) {
 
 app.controller('ProductController', function ($scope, $http, $route, $routeParams) {
     var resorcesToLoad = [
-        'availability',
-        'description',
-        'gallery',
-        'price',
-        'reviews'
+        ['availability', 'json'],
+        ['description', 'json'],
+        ['gallery', 'json'],
+        ['price', 'json'],
+        ['reviews', 'json'],
+        ['specs', 'html']
     ];
 
     for (var res in resorcesToLoad) {
-      requestData("product/"+$routeParams.part_number+"/"+resorcesToLoad[res]+".json").then(function(data){
-        $scope.data = angular.fromJson(data);
-      },function(err){
-        var responsePromise = $http.get('http://api.hack1.smart-things.ro/'+err.resource);
-        responsePromise.success(function (data, status, headers, config) {
-            var last = config.url.split("/");
-            last = last[last.length - 1];
-            last = last.split(".");
-            last = last[0];
-            $scope[last] = angular.fromJson(data);
-            saveData(err.resource,data);
+        requestData("product/" + $routeParams.part_number + "/" + resorcesToLoad[res][0] + "." + resorcesToLoad[res][1]).then(function (data) {
+            if (resorcesToLoad[res][1] == 'json') {
+                $scope.data = angular.fromJson(data);
+            } else {
+                $scope.data = data;
+            }
+        }, function (err) {
+            var responsePromise = $http.get('http://api.hack1.smart-things.ro/' + err.resource);
+            responsePromise.success(function (data, status, headers, config) {
+                var fileName = config.url.split("/");
+                fileName = fileName[fileName.length - 1];
+                fileName = fileName.split(".");
+                var last = fileName[0];
+                var ext = fileName[1];
+                if (ext == 'json') {
+                    $scope[last] = angular.fromJson(data);
+                } else {
+                    $scope[last] = data;
+                }
+                saveData(err.resource, data);
+            });
+            responsePromise.error(function (data, status, headers, config) {
+                alert("AJAX failed!");
+            });
         });
-        responsePromise.error(function (data, status, headers, config) {
-            alert("AJAX failed!");
-        });
-      });
     }
 });
 
-setTimeout(function() {
-  angular.bootstrap(document, ['eMAG-SP-RTC']);
+setTimeout(function () {
+    angular.bootstrap(document, ['eMAG-SP-RTC']);
 }, 1000);
