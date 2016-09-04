aca149cbe77507322cd0356673c0c87e38cdc5c9
MWP2 <mwp2@emag.ro>
2016-09-04 01:34:05 +0000

    Product page changes

--- app.js
+++ app.js
@@ -29,36 +29,43 @@ app.controller('HomeController', function ($scope, $http) {
     loadScript("index_files/homepage.js");
 });
 
-app.controller('ProductController', function ($scope, $http, $route, $routeParams) {
-    var resorcesToLoad = [
-        ['availability', 'json'],
-        ['description', 'json'],
-        ['gallery', 'json'],
-        ['price', 'json'],
-        ['reviews', 'json'],
-        ['specs', 'html']
-    ];
 
-    for (var res in resorcesToLoad) {
-        requestData("product/" + $routeParams.part_number + "/" + resorcesToLoad[res][0] + "." + resorcesToLoad[res][1]).then(function (data) {
-            $scope.data = data;
-        }, function (err) {
-            var responsePromise = $http.get('http://api.hack1.smart-things.ro/' + err.resource);
-            responsePromise.success(function (data, status, headers, config) {
-                var fileName = config.url.split("/");
-                fileName = fileName[fileName.length - 1];
-                fileName = fileName.split(".");
-                var last = fileName[0];
-                var ext = fileName[1];
-                $scope[last] = data;
-                saveData(err.resource, data);
+app.controller('ProductController',['$scope', '$http', '$sce', '$routeParams',
+    function ($scope, $http, $sce, $routeParams) {
+        $scope.renderHtml = function (htmlCode) {
+            return $sce.trustAsHtml(htmlCode);
+        };
+
+        var resorcesToLoad = [
+            ['availability', 'json'],
+            ['description', 'json'],
+            ['gallery', 'json'],
+            ['price', 'json'],
+            ['reviews', 'json'],
+            ['specs', 'html']
+        ];
+
+        for (var res in resorcesToLoad) {
+            requestData("product/" + $routeParams.part_number + "/" + resorcesToLoad[res][0] + "." + resorcesToLoad[res][1]).then(function (data) {
+                $scope.data = data;
+            }, function (err) {
+                var responsePromise = $http.get('http://api.hack1.smart-things.ro/' + err.resource);
+                responsePromise.success(function (data, status, headers, config) {
+                    var fileName = config.url.split("/");
+                    fileName = fileName[fileName.length - 1];
+                    fileName = fileName.split(".");
+                    var last = fileName[0];
+                    var ext = fileName[1];
+                    $scope[last] = data;
+                    saveData(err.resource, data);
+                });
+                responsePromise.error(function (data, status, headers, config) {
+                    alert("AJAX failed!");
+                });
             });
-            responsePromise.error(function (data, status, headers, config) {
-                alert("AJAX failed!");
-            });
-        });
-    }
-});
+        }
+    }]
+);
 
 setTimeout(function () {
     angular.bootstrap(document, ['eMAG-SP-RTC']);
